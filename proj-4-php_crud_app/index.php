970120300cfa702174a8953cb069a032bd787a04
wbrymo <111048430+wbrymo@users.noreply.github.com>
2025-03-30 15:44:29 +0100

    Update index.php

--- proj-4-php_crud_app/index.php
+++ proj-4-php_crud_app/index.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -11,7 +10,7 @@ $db = 'studentdb';
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-  die('Connection failed: ' . $conn->connect_error);
+    die('Connection failed: ' . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM students";
@@ -19,9 +18,23 @@ $result = $conn->query($sql);
 
 echo "<h1>PHP CRUD App - Almost a DevOps Engineer!</h1>";
 echo "<h1>This application was built by WALE, RUKKY & TIMI</h1>";
-echo "<table border='1'><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+echo "<a href='create.php'>➕ Add New Student</a><br><br>";
+
+echo "<table border='1'>
+        <tr>
+            <th>ID</th><th>Name</th><th>Email</th><th>Actions</th>
+        </tr>";
+
 while($row = $result->fetch_assoc()) {
-  echo "<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['email']}</td></tr>";
+    echo "<tr>
+            <td>{$row['id']}</td>
+            <td>{$row['name']}</td>
+            <td>{$row['email']}</td>
+            <td>
+                <a href='edit.php?id={$row['id']}'>✏️ Edit</a> |
+                <a href='delete.php?id={$row['id']}' onclick='return confirm(\"Are you sure you want to delete this record?\")'>🗑️ Delete</a>
+            </td>
+          </tr>";
 }
 echo "</table>";
 
